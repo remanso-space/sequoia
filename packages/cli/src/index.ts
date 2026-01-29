34c881434a4b6a288a909418b3be7387c4461884
Steve <contact@stevedylan.dev>
2026-01-29 15:52:39 -0500

    chore: version bump

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -33,7 +33,7 @@ Publish evergreen content to the ATmosphere
 
 > https://tanlged.org/stevedylan.dev/sequoia
 	`,
-	version: "0.0.5",
+	version: "0.0.6",
 	cmds: {
 		auth: authCommand,
 		init: initCommand,
